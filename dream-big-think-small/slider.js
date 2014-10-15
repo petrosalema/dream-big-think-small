1c21390a388f627dacb0961ddeb1aea4b8135b88
Petro Salema <p.salema@gentics.com>
2014-10-15 07:50:36 +0200

    New website, new talk

---
+++ dream-big-think-small/slider.js
@@ -0,0 +1,118 @@
+// slider.js -- copyright 2014 petro salema
+(function (global, $) {
+	'use strict';
+
+	var CYCLE = true;
+	var $slides = $('.slides>li');
+	var current = 0;
+	var min_index = 0;
+	var max_index = $slides.length - 1;
+
+	// reverse z-index stack so that first slide is on top
+	$slides.each(function (i) {
+		$(this).css('z-index', max_index - i).data('slide-index', i);
+		if (i > 0) {
+			$(this).css('display', 'none');
+		}
+	});
+
+	function call(commands, $prev, $next, callback) {
+		(function next() {
+			if (0 === commands.length) {
+				return callback($prev, $next);
+			}
+			var action = window.slides_actions[commands.shift()];
+			if (action) {
+				action($prev, $next, next);
+			} else {
+				next();
+			}
+		}());
+	}
+
+	function leave($prev, $next, callback) {
+		if (!$prev) {
+			if (callback) {
+				callback();
+			}
+			return;
+		}
+		$prev.removeClass('active');
+		var commands = $prev.attr('data-on-leave');
+		call(commands ? commands.split(',') : [], $prev, $next, callback);
+	}
+
+	function enter($prev, $next, callback) {
+		if (!$next) {
+			if (callback) {
+				callback();
+			}
+			return;
+		}
+		var commands = $next.attr('data-on-enter');
+		call(commands ? commands.split(',') : [], $prev, $next, shuffle);
+		setTimeout(function () {
+			$next.addClass('active');
+		}, 10);
+	}
+
+	function shuffle($prev, $next) {
+		$slides.css('z-index', min_index);
+		if ($prev) {
+			$prev.stop().css('z-index', max_index -1);
+		}
+		$next.stop().css('z-index', max_index).show();
+	}
+
+	function transition(prev, next) {
+		leave(-1 === prev ? null : $slides.eq(prev), $slides.eq(next), enter);
+	}
+
+	function prev(index, cycle) {
+		var new_index = index - 1;
+		return new_index >= min_index ? new_index : cycle ? max_index : min_index;
+	}
+
+	function next(index, cycle) {
+		var new_index = index + 1;
+		return new_index <= max_index ? new_index : cycle ? min_index : max_index;
+	}
+
+	function key_down($event) {
+		var old = current;
+		switch ($event.keyCode) {
+		case 37: // ←
+		case 38: // ↑
+			current = prev(old, CYCLE);
+			break;
+		case 32: // [space] 
+		case 39: // → 
+		case 40: // ↓
+		case 13: // ←┘
+			current = next(old, CYCLE);
+			break;
+		case 27: // [esc]
+			return;
+		default:
+			return;
+		}
+		if (current !== old) {
+			transition(old, current);
+		}
+	}
+
+	function mouse_down($event) {
+		var old = current;
+		current = next(old, CYCLE);
+		transition(old, current, 'forward');
+	}
+
+	if (window.location.hash) {
+		current = parseInt(location.hash.replace('#', '') || 0, 10) || current;
+	}
+
+	$(document).on('keydown', key_down).on('mousedown', mouse_down).ready(function () {
+		transition(-1, current);
+	});
+
+}(window, window.jQuery));
--- main.js
+++ main.js
@@ -1,236 +1,52 @@
 (function () {
+
 	'use strict';
 
-	// --- `polyfills ---
-
-	var requestAnimationFrame = (function () {
-		return window.requestAnimationFrame
-		    || window.webkitRequestAnimationFrame
-		    || window.mozRequestAnimationFrame
-		    || window.oRequestAnimationFrame
-		    || window.msRequestAnimationFrame
-		    || function (callback) {
-				window.setTimeout(callback, 1000 / 60);
-			};
-	}());
-
-	if (window.CanvasRenderingContext2D && !CanvasRenderingContext2D.createImageData) {
-		CanvasRenderingContext2D.prototype.createImageData = function (w, h) {
-			return this.getImageData(0, 0, w, h);
-		};
+	function fitToWidth($inner, $outer) {
+		var size = $outer.width();
+		$inner.css({
+			width  : size,
+			height : size * ($inner.height() / $inner.width())
+		});
+		$inner.css({
+			top  : ($outer.height() - $inner.height()) / 2,
+			left : 0
+		});
 	}
 
-	// --- /polyfills ---
-
-
-	var omx = 0;
-	var omy = 0;
-	var mx = 0;
-	var my = 0;
-	var width;
-	var height;
-	var density = 100;
-	var resolution = window.fieldRes = 16 * 3;
-	var bg = Colors.rgb('#fff');
-	var fg = Colors.rgb('#111');
-	var stopped = false;
-	var running = false;
-	var paused = false;
-
-	function isBetween(value, min, max) {
-		return value >= min && value <= max;
+	function fitToHeight($inner, $outer) {
+		var size = $outer.height();
+		var ratio = $inner.width() / $inner.height();
+		$inner.css({
+			width  : size * ($inner.width() / $inner.height()),
+			height : size
+		});
+		$inner.css({
+			top  : 0,
+			left : ($outer.width() - $inner.width()) / 2
+		});
 	}
 
-	function magnitude(x, y) {
-		return Math.sqrt(x * x + y * y);
-	}
-
-	function prepareFrame(field) {
-		if (paused || !isBetween(mx, 0, width) || !isBetween(my, 0, height)) {
-			return;
-		}
-		var w = field.width();
-		var h = field.height();
-		var dx = mx - omx;
-		var dy = my - omy;
-		var length = Math.max(1, magnitude(dx, dy));
-		var i;
-		for (i = 0; i < length; i++) {
-			var x = (((omx + dx * (i / length)) / width) * w) | 0;
-			var y = (((omy + dy * (i / length)) / height) * h) | 0;
-			field.setVelocity(x, y, dx, dy);
-			field.setDensity(x, y, density);
-		}
-		omx = mx;
-		omy = my;
-	}
-
-	function start(field) {
-		running = true;
-		(function step() {
-			field.update();
-			if (running) {
-				requestAnimationFrame(step);
-			}
-		}());
-	}
-
-	function stop() {
-		running = false;
-		stopped = true;
-	}
-
-	function end(color, canvas) {
-		canvas.parentNode.style.background = Colors.toString(color);
-		setTimeout(function () {
-			canvas.style.opacity = 0;
-		}, 500);
-	}
-
-	function getOffsets(element) {
-		var top = 0;
-		var left = 0;
-		do {
-			top += element.offsetTop;
-			left += element.offsetLeft;
-		} while (element = element.offsetParent);
-		return {
-			left : left,
-			top  : top
-		};
-	}
-
-    function render(field) {
-		if (paused) {
-			return;
-		}
-        var context = field.canvas.getContext('2d');
-		var touched = 0;
-        var w = field.width();
-        var h = field.height();
-		var x;
-		var y;
-		for (x = 0; x < w; x++) {
-			for (y = 0; y < h; y++) {
-				var intensity = Math.min(1, field.getDensity(x, y));
-				context.fillStyle = Colors.toString(Colors.cross(bg, fg, intensity));
-				context.fillRect(x, y, 1, 1);
-				if (intensity >= 0.9) {
-					touched++;
-				}
+	$(function () {
+		var $inner = $('.cover');
+		var $outer = $inner.parent();
+		var timer;
+		function cover() {
+			var diffW = $outer.width() - $inner.width();
+			var diffH = $outer.height() - $inner.height();
+			if (diffW > diffH) {
+				fitToWidth($inner, $outer);
+			} else {
+				fitToHeight($inner, $outer);
 			}
 		}
-		if (touched / (w * h) > 0.8) {
-			end(fg, field.canvas);
-		}
-		if (touched / (w * h) >= 0.95) {
-			stop();
-		}
-    }
-
-	window.onload = function () {
-		var canvas = document.querySelector('#canvas');
-		var style = getComputedStyle(canvas.parentNode);
-
-		width = parseInt(style.width, 10);
-		height = parseInt(style.height, 10);
-
-		canvas.style.height = height + 'px';
-
-		var aspect = Math.round(width / height);
-		var field = new FluidField(canvas);
-		field.setUICallback(prepareFrame);
-		field.setDisplayFunction(render);
-		field.setResolution(resolution, resolution * aspect);
-
-		var interacting = false;
-		var interact = function (event) {
-			if (stopped) {
-				return;
+		$(window).on('resize', function () {
+			if (timer) {
+				clearTimeout(timer);
 			}
-			var scrollLeft = window.pageXOffset - document.body.clientLeft;
-			var scrollTop  = window.pageYOffset - document.body.clientTop;
+			timer = setTimeout(cover, 100);
+		});
+		cover();
+	});
 
-			paused = scrollTop > height;
-
-			if (paused) {
-				return;
-			}
-
-			var offset = getOffsets(canvas);
-			mx = event.clientX - offset.left + scrollLeft;
-			my = event.clientY - offset.top + scrollTop;
-			if (!running) {
-				omx = mx;
-				omy = my;
-				start(field);
-			}
-			interacting = true;
-		};
-
-		document.onmousemove = interact;
-		document.onmousedown = interact;
-
-		var waypoints = [
-			{
-				x: width / 2,
-				y: height * 0.75
-			}, {
-				x: width / 2,
-				y: height * 0.25
-			}, {
-				x: width * 0.75,
-				y: height / 2
-			}, {
-				x: width * 0.25,
-				y: height / 2
-			}
-		];
-
-		function puff() {
-			if (interacting) {
-				return;
-			}
-			var point = waypoints.pop();
-			if (point) {
-				mx = point.x;
-				my = point.y;
-				setTimeout(puff, 1000);
-			}
-			if (!running) {
-				omx = width / 2;
-				omy = height / 2;
-				start(field);
-			}
-		}
-
-		setTimeout(puff, 1000);
-
-		setTimeout(function () {
-			end(fg, canvas);
-			setTimeout(stop, 2000);
-		}, 5000);
-
-		var flipcards = [].slice.call(document.querySelectorAll('.flipcard'));
-		flipcards.forEach(Flipcards.create);
-	};
-
-	if (window.console && 'function' === typeof window.console.log) {
-		var colophon = '@petrosalema • colophon\n'
-		             + '▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n'
-		             + '\n'
-		             + 'Fluid Dynamics\n'
-		             + ' • www.nerget.com/fluidSim/\n'
-		             + ' • code.google.com/p/javascript-fluid/\n'
-		             + ' • www.cs.ubc.ca/~rbridson/docs/brentw_msc.pdf\n'
-		             + ' • www.cs.ubc.ca/~rbridson/docs/batty-sca08-viscosity.pdf\n'
-		             + ' • www.cs.ubc.ca/~rbridson/docs/schechter-sca08-turbulence.pdf\n'
-		             + ' • www.dgp.toronto.edu/people/stam/reality/Research/pdf/GDC03.pdf\n'
-		             + ' • www.stackoverflow.com/questions/4893992/fluid-dynamic-simulation-with-obstacles\n'
-		             + '\n'
-		             + 'Type Faces\n'
-					 + ' • Title/Headings: Helvetica\n'
-					 + ' • Content: FastCompany\'s Zizou Slab\n';
-		console.log(colophon);
-	}
 }());
